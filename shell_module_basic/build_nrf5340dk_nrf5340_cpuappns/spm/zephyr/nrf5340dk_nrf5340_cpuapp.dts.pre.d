f762bbff5c67bc27a006be9679225742d13caf37
Mario <mario_hn@hotmail.com>
2021-08-03 12:48:43 +0200

    Added secure module (non-functional)

---
+++ shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/zephyr/nrf5340dk_nrf5340_cpuapp.dts.pre.d
@@ -0,0 +1,16 @@
+empty_file.o: C:/ncs/v1.6.0/zephyr/misc/empty_file.c \
+ C:/ncs/v1.6.0/zephyr/boards/arm/nrf5340dk_nrf5340/nrf5340dk_nrf5340_cpuapp.dts \
+ C:/ncs/v1.6.0/zephyr/dts/arm/nordic/nrf5340_cpuapp_qkaa.dtsi \
+ C:/ncs/v1.6.0/zephyr/dts/common/mem.h \
+ C:/ncs/v1.6.0/zephyr/dts/arm/nordic/nrf5340_cpuapp.dtsi \
+ C:/ncs/v1.6.0/zephyr/dts/arm/armv8-m.dtsi \
+ C:/ncs/v1.6.0/zephyr/dts/common/skeleton.dtsi \
+ C:/ncs/v1.6.0/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ C:/ncs/v1.6.0/zephyr/include/dt-bindings/i2c/i2c.h \
+ C:/ncs/v1.6.0/zephyr/include/dt-bindings/gpio/gpio.h \
+ C:/ncs/v1.6.0/zephyr/dts/arm/nordic/override.dtsi \
+ C:/ncs/v1.6.0/zephyr/dts/arm/nordic/nrf5340_cpuapp_peripherals.dtsi \
+ C:/ncs/v1.6.0/zephyr/boards/arm/nrf5340dk_nrf5340/nrf5340_cpuapp_common.dts \
+ C:/ncs/v1.6.0/zephyr/boards/arm/nrf5340dk_nrf5340/nrf5340_cpuapp_partition_conf.dts \
+ C:/ncs/v1.6.0/zephyr/boards/arm/nrf5340dk_nrf5340/nrf5340_shared_sram_planning_conf.dts \
+ C:/ncs/v1.6.0/nrf/samples/spm/boards/nrf5340dk_nrf5340_cpuapp.overlay
