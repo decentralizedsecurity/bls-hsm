4dd4e75ce5aced99cda6f7ff3242e0ab1437c213
Isaac <isaacagudo@gmail.com>
2021-08-23 20:03:31 +0200

    Revert "Delete shell_module_basic/build_nrf5340dk_nrf5340_cpuappns directory"
    This reverts commit 03fc7b3866fc1cc3921c12b0275498f8e5f887ee.

---
+++ shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/zephyr/cmake_install.cmake
@@ -0,0 +1,78 @@
+# Install script for directory: C:/ncs/v1.6.0/zephyr
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Zephyr-Kernel")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "TRUE")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/zephyr/arch/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/zephyr/lib/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/zephyr/soc/arm/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/zephyr/boards/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/zephyr/subsys/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/zephyr/drivers/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/nrf/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/openthread/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/mcuboot/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/trusted-firmware-m/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/cjson/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/pelion-dm/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/cddl-gen/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/memfault-firmware-sdk/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/cmsis/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/canopennode/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/civetweb/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/fatfs/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/hal_nordic/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/st/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/libmetal/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/lvgl/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/mbedtls/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/mcumgr/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/open-amp/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/loramac-node/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/segger/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/tinycbor/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/tinycrypt/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/littlefs/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/mipi-sys-t/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/nrf_hw_models/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/TraceRecorder/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/nrfxlib/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/modules/connectedhomeip/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/zephyr/kernel/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/zephyr/cmake/flash/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/zephyr/cmake/usage/cmake_install.cmake")
+  include("C:/Users/mario/OneDrive/Escritorio/bls-hsm/shell_module_basic/build_nrf5340dk_nrf5340_cpuappns/spm/zephyr/cmake/reports/cmake_install.cmake")
+
+endif()
+
