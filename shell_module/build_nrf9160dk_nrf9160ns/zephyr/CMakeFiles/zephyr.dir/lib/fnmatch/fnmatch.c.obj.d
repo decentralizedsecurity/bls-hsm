bb48814d5dfb1923271df6b11e1bd77e8ef98bc9
isaacagudo <isaacagudo@gmail.com>
2021-08-23 19:42:55 +0200

    Delete shell_module/build_nrf9160dk_nrf9160ns directory

--- shell_module/build_nrf9160dk_nrf9160ns/zephyr/CMakeFiles/zephyr.dir/lib/fnmatch/fnmatch.c.obj.d
+++
@@ -1,9 +0,0 @@
-zephyr/CMakeFiles/zephyr.dir/lib/fnmatch/fnmatch.c.obj: \
- C:/Users/pablo/ncs/v1.4.99-dev1/zephyr/lib/fnmatch/fnmatch.c \
- C:/Users/pablo/ncs/v1.4.99-dev1/zephyr/samples/subsys/shell/shell_module/build_nrf9160dk_nrf9160ns/zephyr/include/generated/autoconf.h \
- C:/Users/pablo/ncs/v1.4.99-dev1/zephyr/include/toolchain/zephyr_stdint.h \
- ../../../../../lib/libc/minimal/include/ctype.h \
- C:/Users/pablo/ncs/v1.4.99-dev1/zephyr/lib/fnmatch/fnmatch.h \
- ../../../../../lib/libc/minimal/include/string.h \
- c:\users\pablo\ncs\v1.4.99-dev1\toolchain\opt\lib\gcc\arm-none-eabi\9.2.1\include\stddef.h \
- ../../../../../lib/libc/minimal/include/bits/restrict.h
--- shell_module/build_nrf9160dk_nrf9160ns/zephyr/CMakeFiles/zephyr_final.dir/misc/empty_file.c.obj.d
+++
@@ -1,4 +0,0 @@
-zephyr/CMakeFiles/zephyr_final.dir/misc/empty_file.c.obj: \
- C:/Users/pablo/ncs/v1.4.99-dev1/zephyr/misc/empty_file.c \
- C:/Users/pablo/ncs/v1.4.99-dev1/zephyr/samples/subsys/shell/shell_module/build_nrf9160dk_nrf9160ns/zephyr/include/generated/autoconf.h \
- C:/Users/pablo/ncs/v1.4.99-dev1/zephyr/include/toolchain/zephyr_stdint.h
