c9ff1ed57fb44daaa4e232557ca820ffd640a51d
Mario <mario_hn@hotmail.com>
2021-08-23 20:04:45 +0200

    Deleted build folders

--- shell_module_basic/build_nrf9160dk_nrf9160ns/modules/nrfxlib/nrf_security/src/mbedtls/shared/CMakeFiles/mbedcrypto_shared.dir/C_/Users/pablo/ncs/v1.4.99-dev1/zephyr/misc/empty_file.c.obj.d
+++
@@ -1,2 +0,0 @@
-modules/nrfxlib/nrf_security/src/mbedtls/shared/CMakeFiles/mbedcrypto_shared.dir/C_/Users/pablo/ncs/v1.4.99-dev1/zephyr/misc/empty_file.c.obj: \
- C:/Users/pablo/ncs/v1.4.99-dev1/zephyr/misc/empty_file.c
--- shell_module_basic/build_nrf9160dk_nrf9160ns/zephyr/lib/libc/minimal/CMakeFiles/lib__libc__minimal.dir/source/time/gmtime.c.obj.d
+++
@@ -1,9 +0,0 @@
-zephyr/lib/libc/minimal/CMakeFiles/lib__libc__minimal.dir/source/time/gmtime.c.obj: \
- C:/Users/pablo/ncs/v1.4.99-dev1/zephyr/lib/libc/minimal/source/time/gmtime.c \
- C:/Users/pablo/ncs/v1.4.99-dev1/zephyr/samples/subsys/shell/shell_module_basic/build_nrf9160dk_nrf9160ns/zephyr/include/generated/autoconf.h \
- C:/Users/pablo/ncs/v1.4.99-dev1/zephyr/include/toolchain/zephyr_stdint.h \
- ../../../../../lib/libc/minimal/include/time.h \
- ../../../../../lib/libc/minimal/include/stdint.h \
- ../../../../../lib/libc/minimal/include/sys/_types.h \
- ../../../../../lib/libc/minimal/include/bits/restrict.h \
- ../../../../../lib/libc/minimal/include/sys/_timespec.h
