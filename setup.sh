fc1c9fa5fa36383276da770e686e33627f60b9fe
Mario <mario_hn@hotmail.com>
2021-09-29 15:20:12 +0200

    Update setup.sh
    Adds option -i which installs required ARM cross-compiler

--- setup.sh
+++ setup.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
 
-while getopts ":c:" opt; do
+usage(){
+  echo "Usage: $0 [-c \"compiler path\"] [-i]
+  -c \"compiler path\": define path of the arm compiler (arm-none-eabi-gcc file)
+  -i: check if GNU ARM Embedded Toolchain is installed. Install it otherwise"
+  exit 1;
+}
+
+while getopts ":c::i" opt; do
   case $opt in
     c) comp="$OPTARG"
     ;;
-    \?) echo "Invalid option -$OPTARG" >&2
-    exit 1
+    i) sudo apt install gcc-arm-none-eabi
+    comp="/usr/bin/arm-none-eabi-gcc"
     ;;
-  esac
-
-  case $OPTARG in
-    -*) echo "Option $opt needs a valid argument"
-    exit 1
+    *) usage
     ;;
   esac
 done
+
+if [ -z $comp ]; then
+  usage
+fi
 
 echo "Compiler selected: $comp"
 
 ./blst/build.sh CC=$comp -mcpu=cortex-m33 flavour=elf -fno-pie
 ret=$?
 
-if [ $ret -eq 0 ];
-then
-echo "Blst library builded"
-rm -f ./cli/include/blst.h
-rm -f ./cli/include/blst_aux.h
-rm -f ./cli/lib/libblst.a
-mkdir -p ./cli/lib
-mv ./libblst.a ./cli/lib/
-mkdir -p ./cli/include
-cp ./blst/bindings/blst.h ./blst/bindings/blst_aux.h ./cli/include/
-
+if [ $ret -eq 0 ]; then
+  echo "Blst library built"
+  rm -f ./cli/include/blst.h
+  rm -f ./cli/include/blst_aux.h
+  rm -f ./cli/lib/libblst.a
+  mkdir -p ./cli/lib
+  mv ./libblst.a ./cli/lib/
+  mkdir -p ./cli/include
+  cp ./blst/bindings/blst.h ./blst/bindings/blst_aux.h ./cli/include/
 else
-echo "Error building blst library"
-
+  echo "Error building blst library"
 fi
