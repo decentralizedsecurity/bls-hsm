fdab642842979e387304f5bdb07530da7075bc5b
Mario <mario_hn@hotmail.com>
2021-10-20 15:49:22 +0200

    Update utils.c

--- emu/utils.c
+++ emu/utils.c
@@ -166,15 +166,18 @@ int pk_parse(char* pk_hex, blst_p1_affine* pk){
         int error = 0;
 
         if(offset == -1){
-            printf("Incorrect public key length. It must be 96 characters long.\n");
+            //printf("Incorrect public key length. It must be 96 characters long.\n");
+            strcat(buff, "Incorrect public key length. It must be 96 characters long.\n");
             error = 1;
         }else{
             if(char_chk(pk_hex + offset, 96)){
-                printf("Public key contains incorrect characters.\n");
+                //printf("Public key contains incorrect characters.\n");
+                strcat(buff, "Public key contains incorrect characters.\n");
                 error = 1;
             }else{
                 if(hex2bin(pk_hex + offset, 96, pk_bin, 48) == 0) {
-                    printf("Failed converting public key to binary array\n");
+                    //printf("Failed converting public key to binary array\n");
+                    strcat(buff, "Failed converting public key to binary array\n");
                     error = 1;
                 }else{
                     blst_p1_uncompress(pk, pk_bin);
@@ -191,15 +194,18 @@ int msg_parse(char* msg, uint8_t* msg_bin){
         int error = 0;
 
         if(offset == -1){
-            printf("Incorrect message length. It must be 64 characters long.\n");
+            //printf("Incorrect message length. It must be 64 characters long.\n");
+            strcat(buff, "Incorrect message length. It must be 64 characters long.\n");
             error = 1;
         }else{
             if(char_chk(msg + offset, 64)){
-                printf("Message contains incorrect characters.\n");
+                //printf("Message contains incorrect characters.\n");
+                strcat(buff, "Message contains incorrect characters.\n");
                 error = 1;
             }else{
                 if(hex2bin(msg + offset, 64, msg_bin, 32) == 0) {
-                    printf("Failed converting message to binary array\n");
+                    //printf("Failed converting message to binary array\n");
+                    strcat(buff, "Failed converting message to binary array\n");
                     error = 1;
                 }
             }
@@ -214,15 +220,18 @@ int sig_parse(char* sig_hex, blst_p2_affine* sig){
         int error = 0;
 
         if(offset == -1){
-            printf("Incorrect signature length. It must be 192 characters long.\n");
+            //printf("Incorrect signature length. It must be 192 characters long.\n");
+            strcat(buff, "Incorrect signature length. It must be 192 characters long.\n");
             error = 1;
         }else{
             if(char_chk(sig_hex + offset, 192)){
-                printf("Signature contains incorrect characters.\n");
+                //printf("Signature contains incorrect characters.\n");
+                strcat(buff, "Signature contains incorrect characters.\n");
                 error = 1;
             }else{
                 if(hex2bin(sig_hex + offset, 192, sig_bin, 96) == 0) {
-                    printf("Failed converting signature to binary array\n");
+                    //printf("Failed converting signature to binary array\n");
+                    strcat(buff, "Failed converting signature to binary array\n");
                     error = 1;
                 }else{
                     blst_p2_uncompress(sig, sig_bin);
