b7df8c50432b3c12bb9b1e2401fa1d45dbe7710a
Mario-DLH <mario_hn@hotmail.com>
2022-03-24 20:36:43 +0100

    Update directory structure and scripts
    Move libraries to lib folder.
    Add script for cloning external libraries

---
+++ build_libs.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/bash
+
+usage(){
+  echo "Usage: $0 [-c \"compiler path\"] [-h]
+  -c \"compiler path\": define path of the arm compiler (arm-none-eabi-gcc file)
+  -h: display this info"
+  exit 1;
+}
+
+while getopts ":c::h" opt; do
+  case $opt in
+    c) comp="$OPTARG"
+    ;;
+    h) usage
+    ;;
+  esac
+done
+
+
+if [[ ! -f ./lib/libblst.a ]] || [[ ! -f ./lib/blst.h ]] || [[ ! -f ./lib/blst_aux.h ]]; then
+  echo "Cloning blst library"
+  #Use working version of blst module
+  git clone https://github.com/supranational/blst.git
+
+  if [ -z $comp ]; then
+	  echo "No compiler was selected. Blst library will be built with default compiler."
+    ./blst/build.sh
+  else
+    ./blst/build.sh CC=$comp -mcpu=cortex-m33 flavour=elf -fno-pie
+  fi
+  ret=$?
+
+  if [ $ret -eq 0 ]; then
+    echo "Blst library built"
+    sudo mv ./libblst.a ./lib/
+    sudo cp ./blst/bindings/blst.h ./blst/bindings/blst_aux.h ./lib/
+  else
+    echo "Error building blst library"
+  fi
+fi
+
+if [[ ! -f ./lib/picohttpparser.c ]] || [[ ! -f ./lib/picohttpparser.h ]]; then
+  echo "Cloning picohttpparser library"
+  git clone https://github.com/h2o/picohttpparser.git
+  sudo cp ./picohttpparser/picohttpparser.c ./picohttpparser/picohttpparser.h ./lib/
+fi
+
+if [[ ! -f ./lib/cJSON.c ]] || [[ ! -f ./lib/cJSON.h ]]; then
+  echo "Cloning cJSON library"
+  git clone https://github.com/DaveGamble/cJSON.git
+  sudo cp ./cJSON/cJSON.c ./cJSON/cJSON.h ./lib/
+fi
+
+echo "Done"
