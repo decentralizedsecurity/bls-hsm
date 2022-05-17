ab445bdc223a86387bb21bc26212ac5c2658a263
Younyat <assouyat1@gmail.com>
2022-05-17 13:20:38 +0200

    clear

--- build_cli-socket.sh
+++ build_cli-socket.sh
@@ -1,12 +1,4 @@
-#git submodule init
-#git submodule update
-#blst/build.sh
-#cp blst/bindings/blst.h blst/bindings/blst_aux.h cli/include/
-#mkdir cli-socket/lib
-#mkdir cli-socket/build
-#mv libblst.a cli-socket/lib/
-#gcc cli-socket/main.c cli-socket/lib/libblst.a -o cli-socket/build/server -Wno-implicit-function-declaration
-#gcc cli-socket/client.c -o cli-socket/build/client -Wno-implicit-function-declaration
+
 
 
 ./build_libs.sh
@@ -15,6 +7,3 @@ gcc cli-socket/main.c  lib/common.c lib/bls_hsm_ns.c  lib/libblst.a -o cli-socke
 gcc cli-socket/client.c -o cli-socket/build/client -Wno-implicit-function-declaration
 
 
-
-#gcc remote-c/main.c lib/bls_hsm.c lib/common.c lib/picohttpparser.c lib/cJSON.c lib/libblst.a lib/libwolfssl.a -o remote-c/build/server -Wno-implicit-function-declaration
-#gcc remote-c/client.c -o remote-c/build/client -Wno-implicit-function-declaration
