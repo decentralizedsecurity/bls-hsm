7645f6db3362c4aa8638792dae2d12c805a508d6
Pablosanserr <pablosanserr@gmail.com>
2022-11-08 12:26:47 +0100

    build_libs.sh updated. This makes a patch in blst with the purpose of using HEAP memory instead of STACK

--- build_libs.sh
+++ build_libs.sh
@@ -1,5 +1,4 @@
-
-
+#!/bin/bash
 usage(){
   echo "Usage: $0 [-c \"compiler path\"] [-h]
   -c \"compiler path\": define path of the arm compiler (arm-none-eabi-gcc file)
@@ -19,8 +18,11 @@ done
 
 if [[ ! -f ./lib/libblst.a ]] || [[ ! -f ./lib/blst.h ]] || [[ ! -f ./lib/blst_aux.h ]]; then
   echo "Cloning blst library"
-  #Use working version of blst module
-  git clone https://github.com/supranational/blst.git
+  #Use version 0.3.10 of blst module
+  git clone --depth 1 --branch v0.3.10 https://github.com/supranational/blst
+
+  #Patch to use HEAP memory instead of STACK
+  printf -- "--- ./blst/src/e2.c     2022-11-03 13:49:53.282533000 +0100\n+++ e2.c        2022-11-03 13:49:25.006248700 +0100\n@@ -497,7 +497,8 @@\n\n     {\n         const byte *scalars[2] = { val.s, NULL };\n-        POINTonE2 table[4][1<<(5-1)];   /* 18KB */\n+        //POINTonE2 table[4][1<<(5-1)];   /* 18KB */\n+        POINTonE2 (*table)[1<<(5-1)] = malloc(4*(1<<(5-1))*sizeof(POINTonE2));\n         size_t i;\n\n         POINTonE2_precompute_w5(table[0], in);\n@@ -510,6 +511,7 @@\n         }\n\n         POINTonE2s_mult_w5(out, NULL, 4, scalars, 64, table);\n+        free(table);\n     }\n\n     vec_zero(val.l, sizeof(val));   /* scrub the copy of SK */" | patch blst/src/e2.c
 
   if [ -z $comp ]; then
 	  echo "No compiler was selected. Blst library will be built with default compiler."
