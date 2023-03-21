dd4d3845cf5614a49512c99e4307537666383691
Pablosanserr <pablosanserr@gmail.com>
2023-03-21 09:50:59 +0100

    Delete deprecated or useless files from main branch

--- keystoredecrypt/build_import_keystore.sh
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/wolfssl/wolfssl.git
-cd wolfssl
-./configure --enable-scrypt
-make
-sudo make install
-cd ..
-
-gcc import_keystore.c cJSON.c -lm -lwolfssl -o import_keystore -Wno-implicit-function-declaration
